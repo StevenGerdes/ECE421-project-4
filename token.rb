081a98aa0875c8cb1caba9aefdd496ad9da5d315
Steven <sgerdes@ualberta.ca>
2014-11-13 15:25:02 -0700

    Added some invariants to game_main

--- token.rb
+++ token.rb
@@ -2,7 +2,7 @@ require './contract_exceptions'
 
 class Token
 
-  attr_accessor color, column, row, selected?
+  attr_accessor color, selected?
   attr_reader value
   def initialize(value)
     @value = value
