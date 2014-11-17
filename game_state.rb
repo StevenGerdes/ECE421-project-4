6d7abd6b3bc603e801528c0d8ed585ce08b0ddf3
unknown <meen@ualberta.ca>
2014-11-17 14:50:27 -0700

    Finished game_state

--- game_state.rb
+++ game_state.rb
@@ -8,10 +8,9 @@ class GameState
       raise PreconditionError
     end
 
-    @rows = rows
-    @columns = columns
-    @board = Array.new(rows)
-    @board.map! { Array.new(columns) }
+    @rows = rows.to_i
+    @columns = columns.to_i
+    @board = Hash.new(nil)
   end
 
   def play(token, row, column)
@@ -19,15 +18,49 @@ class GameState
     invariant
 
     #precondition
-    if row > @rows ||
-        column > @columns ||
+    if (row.respond_to? :to_i && row.to_i >= @rows && row.to_i < 0) ||
+        (column.respond_to? :to_i && column.to_i >= @columns && column.to_i < 0) ||
         token.is_a?(Token) ||
         @board.include?(token)
       raise PreconditionError
     end
 
+    loc_row = row.to_i
+    loc_column = column.to_i
+
+    if column_full? loc_column
+      result = false
+    else
+      @board[row, loc_column] = token
+      @last_played = Coordinate.new(loc_row, loc_column)
+      result = true
+    end
+
     #postcondition
     if !@board.include?(token)
+      raise PostconditionError
+    end
+
+    #invariant
+    invariant
+
+    return result
+  end
+
+  def remove(row, column)
+    #invariant
+    invariant
+
+    #precondition
+    if (row.respond_to? :to_i && row.to_i >= @rows && row.to_i < 0) ||
+        (column.respond_to? :to_i && column.to_i >= @columns && column.to_i < 0)
+      raise PreconditionError
+    end
+
+    @board[row.to_i, column.to_i] = nil
+
+    #postcondition
+    if @board[row, column] != nil
       raise PostconditionError
     end
 
@@ -39,7 +72,14 @@ class GameState
     #invariant
     invariant
 
-    result = nil
+    result = 0
+
+    for i in 0..@rows - 1
+      if @board[i, column].nil?
+        result = i
+      end
+    end
+
     #postcondition
     if result > @rows ||
         result < 0 ||
@@ -49,6 +89,8 @@ class GameState
 
     #invariant
     invariant
+
+    return result
   end
 
   private
@@ -57,5 +99,20 @@ class GameState
         @columns < 0
       raise InvarientError
     end
+  end
+
+  def column_full?(column)
+    if @board[[@rows - 1][column]] != nil
+      return true
+    end
+    return false
+  end
+end
+
+class Coordinate
+  attr_reader row, column
+  def initialize(row, column)
+    @row = row
+    @column = column
   end
 end
