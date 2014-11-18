04604b126580ac97882dadae42e758696069e335
Steven <sgerdes@ualberta.ca>
2014-11-18 15:14:13 -0700

    did stuff

--- win_condition_checker.rb
+++ win_condition_checker.rb
@@ -5,22 +5,27 @@ class WinConditionChecker
 
   class_invariant([])
 
+  def initialize(value)
+    @value = value
+  end
+
   method_contract(
       #preconditions
-      [lambda { | obj, game_state| game_state.respond_to?(:row) },
-       lambda { | obj, game_state| game_state.respond_to?(:column) },
-       lambda { | obj, game_state| game_state.respond_to?(:left_diagonal) },
-       lambda { | obj, game_state| game_state.respond_to?(:right_diagonal) },
-       lambda { | obj, game_state| game_state.respond_to?(:last_played) }],
+      [lambda { |obj, game_state| game_state.respond_to?(:row) },
+       lambda { |obj, game_state| game_state.respond_to?(:column) },
+       lambda { |obj, game_state| game_state.respond_to?(:left_diagonal) },
+       lambda { |obj, game_state| game_state.respond_to?(:right_diagonal) },
+       lambda { |obj, game_state| game_state.respond_to?(:last_played) }],
       #postconditions
-      [lambda { | obj, result, game_state| result.is_a?(TrueClass)|| result.is_a?( FalseClass)}])
+      [lambda { |obj, result, game_state| result.is_a?(TrueClass)|| result.is_a?(FalseClass) }])
 
   def check_win(game_state)
     last_token = game_state.get_token(game_state.last_played)
-    return four_in_row(game_state.row(game_state.last_played.row), last_token.value) ||
-        four_in_row(game_state.column(game_state.last_played.column), last_token.value) ||
-        four_in_row(game_state.left_diagonal(game_state.last_played), last_token.value) ||
-        four_in_row(game_state.right_diagonal(game_state.last_played), last_token.value)
+    return last_token.value == @value ||
+        four_in_row(game_state.row(game_state.last_played.row)) ||
+        four_in_row(game_state.column(game_state.last_played.column)) ||
+        four_in_row(game_state.left_diagonal(game_state.last_played)) ||
+        four_in_row(game_state.right_diagonal(game_state.last_played))
   end
 
   method_contract(
@@ -29,10 +34,10 @@ class WinConditionChecker
       #postconditions
       [])
 
-  def four_in_row(array, value)
+  def four_in_row(array)
     num_in_row = 0
     array.each do |token|
-      if !(token.nil?) && token.value == value
+      if !(token.nil?) && token.value == @value
         num_in_row += 1
         return true if num_in_row == 4
       else
