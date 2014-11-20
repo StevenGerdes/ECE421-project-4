3b0dbe3ae95b8219b0dfe4837a93783c98cfbeed
Steven <sgerdes@ualberta.ca>
2014-11-20 14:46:32 -0700

    formatting

--- assignment4_main.rb
+++ assignment4_main.rb
@@ -4,7 +4,7 @@ require './game_main'
 game = GameMain.new()
 
 if $DEBUG
-    puts 'input <player count> <otto_toot or connect4> Ex: 1 connect4'
-    ans = gets.split
-    game.start_game(ans[0].to_i, ans[1].to_sym)
+  puts 'input <player count> <otto_toot or connect4> Ex: 1 connect4'
+  ans = gets.split
+  game.start_game(ans[0].to_i, ans[1].to_sym)
 end
