5bbca40ab3eaa662154ac69d7c7bcf23e596d634
Steven <sgerdes@ualberta.ca>
2014-11-20 14:15:45 -0700

    changed console start command

--- assignment4_main.rb
+++ assignment4_main.rb
@@ -1,10 +1,10 @@
-#Setting debug to true will set it in debug code. One of the things that happens
-#is a debug console will run
-$DEBUG = true
+#if debug is true the game will be run via a debug console. If it is false it will run the UI
+$DEBUG = false
 require './game_main'
 game = GameMain.new()
 
-#if ui isn't on the game won't start unless you do it yourself
 if $DEBUG
-    game.start_game(1, :otto_toot)
+    puts 'input <player count> <otto_toot or connect4> Ex: 1 connect4'
+    ans = gets.split
+    game.start_game(ans[0].to_i, ans[1].to_sym)
 end
